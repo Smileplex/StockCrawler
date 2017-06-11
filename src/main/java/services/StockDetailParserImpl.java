15cac561380793e20462e6ab09bad0bb5ea6508f
DESKTOP-JEOE7PT\DongwooSeo <game4284@naver.com>
2017-06-11 21:21:02 +0100

    Change in arguments

--- src/main/java/services/StockDetailParserImpl.java
+++ src/main/java/services/StockDetailParserImpl.java
@@ -43,7 +43,7 @@ public class StockDetailParserImpl implements StockDetailParser {
             return;
 
         logProcess();
-        saveStockDetailKeyword(stockKeyword, stockDetailId);
+        saveStockDetailKeyword(stockDetailId, stockKeyword);
     }
 
     private StockInfo getStockInfo(StockKeyword stockKeyword) {
@@ -62,7 +62,7 @@ public class StockDetailParserImpl implements StockDetailParser {
         logger.info(String.format("[%s | %s] updated", stockInfo.getStockName(), stockInfo.getStockCode()));
     }
 
-    private int saveStockDetailKeyword(StockKeyword stockKeyword, int stockDetailId) {
+    private int saveStockDetailKeyword(int stockDetailId, StockKeyword stockKeyword) {
         return stockDetailKeywordDao.save(stockDetailId, stockKeyword.getId());
     }
 
